4ab546e2f09777c4a666ba43f5a7d6ebf7a39456
PurpleGems <PurpleGemJ@gmail.com>
2018-12-20 20:58:57 -0400

    Added a TileWithinRange method inside the player class to check if the players click was within his 3x3 area *only able to plant from the middle of a 3x3 grid around him

--- Joeba/Scripts/Characters/PlayerFarmer.cs
+++ Joeba/Scripts/Characters/PlayerFarmer.cs
@@ -19,7 +19,7 @@ namespace Joeba.Scripts.Characters
     class PlayerFarmer : Component, ITriggerListener, IUpdatable
     {
         int move_speed = 75; // Used to be 50
-
+       
         
 
         public enum FacingDirection
@@ -61,9 +61,12 @@ namespace Joeba.Scripts.Characters
         public override void onAddedToEntity()
         {
             //init animation stuff
-            var texture = entity.scene.content.Load<Texture2D>("Characters/Player/BaseMale");  // This is the spritesheet of our animations
+            var texture = entity.scene.content.Load<Texture2D>("Characters/Player/BaseMale");  // This is the spritesheet of our 
             var textureSplit = Subtexture.subtexturesFromAtlas(texture, 16, 32); // This is the texture split up
-            var col = entity.addComponent(new BoxCollider(-8, 8, 10, 8));
+            
+            //size 10,8
+            var col = entity.addComponent(new BoxCollider(10,8));
+            col.setLocalOffset(new Vector2(-3, 0));
             var camera = entity.addComponent(new FollowCamera(entity, FollowCamera.CameraStyle.LockOn));
             camera.addComponent(new CameraBounds(new Vector2(0, 0), new Vector2(Game1.TileSize * (Game1.CurrentMap.width - 1), Game1.TileSize * (Game1.CurrentMap.height - 1))));
 
@@ -75,7 +78,6 @@ namespace Joeba.Scripts.Characters
             mover = entity.addComponent(new Mover());
             //we add onto our player the component of "Sprite" with animations and we set its first texture
             animations = entity.addComponent(new Sprite<Animations>(textureSplit[0])); // all the other animations collision are based off this also the starting sprite
-
             //heldItem = new WoodenSword();
 
 
@@ -84,7 +86,7 @@ namespace Joeba.Scripts.Characters
                textureSplit[16],
                textureSplit[17],
                textureSplit[18]
-            }));
+            }).setOrigin(new Vector2(8, 28)));
 
             animations.addAnimation(Animations.WalkSouth, new SpriteAnimation(new List<Subtexture>()
             {
@@ -92,7 +94,7 @@ namespace Joeba.Scripts.Characters
                textureSplit[1],
                textureSplit[2]
 
-            }));
+            }).setOrigin(new Vector2(8, 28)));
 
             animations.addAnimation(Animations.WalkEast, new SpriteAnimation(new List<Subtexture>()
             {
@@ -102,7 +104,7 @@ namespace Joeba.Scripts.Characters
                textureSplit[35],
                textureSplit[36],
                textureSplit[37]
-            }));
+            }).setOrigin(new Vector2(8, 28)));
 
             animations.addAnimation(Animations.WalkWest, new SpriteAnimation(new List<Subtexture>()
             {
@@ -112,7 +114,7 @@ namespace Joeba.Scripts.Characters
                textureSplit[51],
                textureSplit[52],
                textureSplit[53]
-            })).setRenderLayer((int)Game1.Layers.PlayerLayer);
+            }).setOrigin(new Vector2(8,28))).setRenderLayer((int)Game1.Layers.PlayerLayer);
 
 
             SetupInput();
@@ -134,20 +136,22 @@ namespace Joeba.Scripts.Characters
         Animations anim = Animations.WalkSouth;
         public void update()
         {
+            
+
             if (Input.isKeyPressed(Keys.P))
             {
                 Vector2 mousePosition = Input.MousePositionInGridTile();
-                if (Game1.HoedSpots.ContainsKey(mousePosition))
+                if (Game1.HoedSpots.ContainsKey(mousePosition) && TileWithinRange())
                 {
                     if (Game1.HoedSpots[mousePosition].getComponent<HoedDirt>().CanPlantSeed())
                     {
                         Game1.HoedSpots[mousePosition].getComponent<HoedDirt>().PlantSeed((int)mousePosition.X, (int)mousePosition.Y, new Crop(0, 6));
                     }
                     else {
-                        Debug.log( "SEED ALREADY PLANTED HERE");
+                        Debug.log( "SEED ALREADY PLANTED HERE - Player Class");
                     }
                 }
-                else { Debug.log("cant plant a seed here, tile hasnt been dug up.");}
+                else { Debug.log("cant plant a seed here, tile hasnt been dug up or too far away. - Player Class");}
             }
 
             var moveDirection = new Vector2(xAxisInput.value, yAxisInput.value);
@@ -175,7 +179,13 @@ namespace Joeba.Scripts.Characters
 
             if (Input.leftMouseButtonPressed)
             {
-                heldItem.OnLeftClick();               
+                //Clickable tiles around the player
+                if (TileWithinRange())
+                heldItem.OnLeftClick();
+                else
+                {
+                    Debug.log("TILE IS TOO FAR AWAY - PlayerFarmerClass");
+                }
             }
 
             if (Input.isKeyPressed(Keys.B))
@@ -190,6 +200,36 @@ namespace Joeba.Scripts.Characters
 
         }
 
+        private bool TileWithinRange()
+        {
+            Vector2 clickedPosition = Input.MousePositionInGridTile();
+            Vector2 playerPosition = Helper.GetTilePosition(entity.position);
+            List<Vector2> clickabletiles = new List<Vector2>();
+
+            clickabletiles.Add(new Vector2(playerPosition.X - 1, playerPosition.Y - 1));
+            clickabletiles.Add(new Vector2(playerPosition.X, playerPosition.Y - 1));
+            clickabletiles.Add(new Vector2(playerPosition.X + 1, playerPosition.Y - 1));
+
+            clickabletiles.Add(new Vector2(playerPosition.X - 1, playerPosition.Y));
+            clickabletiles.Add(new Vector2(playerPosition.X, playerPosition.Y));
+            clickabletiles.Add(new Vector2(playerPosition.X + 1, playerPosition.Y));
+
+            clickabletiles.Add(new Vector2(playerPosition.X - 1, playerPosition.Y + 1));
+            clickabletiles.Add(new Vector2(playerPosition.X, playerPosition.Y + 1));
+            clickabletiles.Add(new Vector2(playerPosition.X + 1, playerPosition.Y + 1));
+
+            
+
+            foreach (var tiles in clickabletiles)
+            {
+                if (clickedPosition == tiles)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         
 
@@ -204,7 +244,6 @@ namespace Joeba.Scripts.Characters
 
         public void onTriggerExit(Collider other, Collider local)
         {
-            Debug.log("triggerExit: {0}", other.entity.name);
 
         }
     }
---
+++ Joeba/Scripts/Items/Tools/Tool.cs
@@ -0,0 +1,25 @@
+﻿using Nez.Sprites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joeba.Scripts.Items.Tools
+{
+    class Tool : Item
+    {
+        public Tool(string name, Sprite sprite) : base(name, sprite)
+        {
+
+        }
+
+
+        public override void OnLeftClick()
+        {
+            
+        }
+
+
+    }
+}
