16fe8636cab7c7f7f8afcfebea67d2758ef3c274
PurpleGems <PurpleGemJ@gmail.com>
2018-12-22 21:03:37 -0400

    Before Trying to implement the character in different parts

--- Joeba/Scripts/Graphics/GlobalSpritesheets.cs
+++ Joeba/Scripts/Graphics/GlobalSpritesheets.cs
@@ -12,11 +12,13 @@ namespace Joeba.Scripts.Graphics
         private static Texture2D Weapons = Core.content.Load<Texture2D>("Items/weapons");
         private static Texture2D Crops = Core.content.Load<Texture2D>("Terrain/Crops/crops");
         private static Texture2D HoedDirt = Core.content.Load<Texture2D>("Terrain/Crops/hoeDirt");
+        private static Texture2D BaseCharacter = Core.content.Load<Texture2D>("Characters/Player/BaseCharacter");
 
         //SplitUp Sheets
         public static List<Subtexture> WeaponsSplit = Subtexture.subtexturesFromAtlas(Weapons, 16, 16);
         public static List<Subtexture> CropsSplit = Subtexture.subtexturesFromAtlas(Crops, 16, 32);
         public static List<Subtexture> HoedDirtSplit = Subtexture.subtexturesFromAtlas(HoedDirt, 16, 16);
+        public static List<Subtexture> BaseCharacterSplit = Subtexture.subtexturesFromAtlas(BaseCharacter, 16, 16);
         
     }
 }
